e79f33a171487516bcc20b85fcdbb54305edbae8
Mocanu Alexandra <mocanu.alexandra1504@gmail.com>
2024-06-22 12:56:50 +0300

    Added url for both local and deployed development

--- angular-project/src/app/services/config.service.ts
+++ angular-project/src/app/services/config.service.ts
@@ -8,6 +8,9 @@ export class ConfigService {
   // Base url used in deployed application
   private _baseURL = 'http://34.205.79.220:80/api';
 
+  // Base url used in local development
+  // private _baseURL = 'http://localhost:80/api';
+
   constructor() { }
 
   get baseURL() {
